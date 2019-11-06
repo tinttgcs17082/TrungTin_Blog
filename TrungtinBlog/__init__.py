5eaafbed20de74f78be7f280a71dcf14b81a4420
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-11-06 12:21:31 +0700

    version 0.0.1

--- TrungtinBlog/__init__.py
+++ TrungtinBlog/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import  Bcrypt
 from flask_login import  LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c6ee238677970bd0480913746877f36b'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db= SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
