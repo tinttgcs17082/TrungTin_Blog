686c8583e9640c2317f98cc33307fe5f3697b356
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-11-07 22:48:38 +0700

    version 1.0.0

--- TrungtinBlog/models.py
+++ TrungtinBlog/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serialized
+from flask import app
 from TrungtinBlog import db, login_manager
 from flask_login import  UserMixin
 
@@ -15,6 +17,20 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
     password = db.Column(db.String(60), nullable = False)
     posts = db.relationship('Post', backref = 'author', lazy = True)
+
+    def get_reset_token(self, expires_sec = 1800):
+        s = Serialized(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serialized(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
 
     def __repr__(self):
         return f"User('{self.fullname}','{self.username}','{self.email}','{self.image_file}')"
